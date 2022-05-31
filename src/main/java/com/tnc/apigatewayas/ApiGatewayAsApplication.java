dd1bb6a72ae4acecd923f086a185be1108a2258b
TncBogdan <tncbogdan@gmail.com>
2022-05-31 13:43:46 +0300

    Initial commit

---
+++ src/main/java/com/tnc/apigatewayas/ApiGatewayAsApplication.java
@@ -0,0 +1,13 @@
+package com.tnc.apigatewayas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayAsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayAsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tnc/apigatewayas/ApiGatewayAsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnc.apigatewayas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayAsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
